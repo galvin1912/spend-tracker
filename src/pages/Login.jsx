b5b1f75b4befa7635f7947e881824a5f19f25b56
Galvin Nguyen <dang.nguyenhai@elinext.com>
2023-12-15 01:01:37 +0700

    update favicon

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { Button, Form, Input } from "antd";
-import LoginImage from "../assets/account-login-protection-8876027-7271014.png"
+import LoginImage from "../assets/account-login-protection-8876027-7271014.png";
 
 const Login = () => {
   const onFinish = (values) => {
@@ -12,16 +12,7 @@ const Login = () => {
 
   return (
     <div className="row">
-      <div className="col-md-6">
-        <h1>Đăng nhập</h1>
-        <p>
-          Đăng nhập để sử dụng các tính năng của hệ thống. Nếu bạn chưa có tài
-          khoản, vui lòng liên hệ với quản trị viên để được cấp quyền truy cập.
-        </p>
-        <img src={LoginImage} alt="Login" className="mw-100" />
-      </div>
-
-      <div className="col-md-6">
+      <div className="col-md-6 order-md-2">
         <Form
           name="basic"
           initialValues={{
@@ -65,6 +56,15 @@ const Login = () => {
             </Button>
           </Form.Item>
         </Form>
+      </div>
+
+      <div className="col-md-6 order-md-1">
+        <h1 className="d-none d-md-block">Đăng nhập</h1>
+        <p>
+          Đăng nhập để sử dụng các tính năng của hệ thống. Nếu bạn chưa có tài
+          khoản, vui lòng liên hệ với quản trị viên để được cấp quyền truy cập.
+        </p>
+        <img src={LoginImage} alt="Login" className="mw-100" />
       </div>
     </div>
   );
